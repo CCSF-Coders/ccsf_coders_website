f9a10fa4e4505f221a0803aae703e600e8666368
otangdec <finalehorizon@gmail.com>
2014-02-19 20:54:40 -0800

    Minor Design and Structure Changes
    - Change the layout the the version 4 mock up
    - relocation the javascript file and css file into their own directories
    - update links

---
+++ js/menu_addon.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function($){
+    $('#mega-menu-red').dcMegaMenu({
+        rowItems: '3',
+        speed: 'fast',
+        effect: 'slide',
+        //event: 'click',
+        fullWidth: false
+    });
+});
