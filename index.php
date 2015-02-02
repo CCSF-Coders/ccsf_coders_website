6655ef37b3a3fdb2a3e67676718e18225a7050e0
William Myers <williamrmyers@gmail.com>
2015-02-01 19:38:59 -0800

    Change from HTML to PHP for hills server

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>CCSF Coders Club</title>
+    <!-- Link to style.css -->
+    <link href='style.css' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Share+Tech+Mono:400' rel='stylesheet' type='text/css'>
+    <style>
+        /* Extra styles go here. */
+    </style>
+
+</head>
+
+<body>
+
+<div id="logo">
+<pre> 
+######################################################################################################################################################################
+
+ ######   ######   ######  ########     ######   #######  ########  ######## ########   ######  
+##    ## ##    ## ##    ## ##          ##    ## ##     ## ##     ## ##       ##     ## ##    ## 
+##       ##       ##       ##          ##       ##     ## ##     ## ##       ##     ## ##       
+##       ##        ######  ######      ##       ##     ## ##     ## ######   ########   ######  
+##       ##             ## ##          ##       ##     ## ##     ## ##       ##   ##         ## 
+##    ## ##    ## ##    ## ##          ##    ## ##     ## ##     ## ##       ##    ##  ##    ## 
+ ######   ######   ######  ##           ######   #######  ########  ######## ##     ##  ######  
+
+ #####################################################################################################################################################################
+</pre>
+</div>
+
+<!-- Include nav.php partial -->
+<?php include 'nav.php'; ?>
+
+<div class="par">
+<h1>About US</h1>
+
+<pre class="content">
+The City College of San Francisco Coders Club is an organization 
+dedicated to providing students with resources and a social network of 
+individuals interested in computer programming. We actively participate in 
+the professional community through meetups, conferences, workshops, talks, and more.
+
+We organize speakers and study groups, share internships and employment 
+opportunities so that we may help our members gain the experiences necessary 
+to start their careers. At our meetings, we provide a fun social environment, 
+where members can develop their coding skills, share projects they are
+ working on, find partners to collaborate with, and enjoy the company of other coders.
+
+Interested in joining? Please visit our <a href="/contact.html">Contact</a> page.
+</pre>
+
+</div>
+
+</body>
