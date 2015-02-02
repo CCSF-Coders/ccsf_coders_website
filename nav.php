360afcda74096599e51e4dfe0542d6e603ac6d77
kevr <kevr@nixcode.us>
2015-02-01 17:04:27 -0800

    Added a nav partial and inlined some php; slight css fixes

---
+++ nav.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "
+<div id='nav'>
+ <ul>
+  <li><a href='index.html'>Home</a></li>
+  <li><a href='index.html'>Blog / News</a></li>
+  <li><a href='index.html'>Toy Box</a></li>
+  <li><a href='http://www.nixcode.us/irc' target='_blank'>IRC</a></li>
+  <li><a href='https://github.com/williamrmyers'>Williams Github</a></li>
+ </ul>
+</div>
+";
+
+?>
+
