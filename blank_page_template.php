c657087c14ba5b52caa7789f2e7ec8d1fde6161a
William Myers <williamrmyers@gmail.com>
2015-02-05 11:07:25 -0800

    Many changes to content and extra pages. Few in any changes to the code. Included a template page for craeting new pages.

---
+++ blank_page_template.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>CCSF Coders Club</title>
+    <!-- Link to style.css -->
+    <link href='style.css' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Share+Tech+Mono:400' rel='stylesheet' type='text/css'>
+    <style>
+        /* Extra styles go here. */
+    </style>
+
+</head>
+
+<body>
+
+<div id="logo">
+<pre> 
+######################################################################################################################################################################
+
+ ######   ######   ######  ########     ######   #######  ########  ######## ########   ######  
+##    ## ##    ## ##    ## ##          ##    ## ##     ## ##     ## ##       ##     ## ##    ## 
+##       ##       ##       ##          ##       ##     ## ##     ## ##       ##     ## ##       
+##       ##        ######  ######      ##       ##     ## ##     ## ######   ########   ######  
+##       ##             ## ##          ##       ##     ## ##     ## ##       ##   ##         ## 
+##    ## ##    ## ##    ## ##          ##    ## ##     ## ##     ## ##       ##    ##  ##    ## 
+ ######   ######   ######  ##           ######   #######  ########  ######## ##     ##  ######  
+
+ #####################################################################################################################################################################
+</pre>
+</div>
+
+<!-- Include nav.php partial -->
+<?php include 'nav.php'; ?>
+
+<div class="par">
+<h1> <!-- Header goes here --> </h1>
+
+<pre class="content">
+
+<!-- Text goes here -->
+
+</pre>
+
+</div>
+
+</body>
