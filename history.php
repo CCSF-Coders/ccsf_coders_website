7b4bd448731069042e8122140e3ec62ec6bfbdba
William Myers <williamrmyers@gmail.com>
2015-02-10 16:21:00 -0800

    Updated the Template pahe with a bottom menu Added a history page if someone wants that.

--- history.php
+++ history.php
@@ -10,6 +10,10 @@
 	<link href='http://fonts.googleapis.com/css?family=Share+Tech+Mono:400' rel='stylesheet' type='text/css'>
     <style>
         /* Extra styles go here. */
+		.content {
+    text-align: left;
+	padding-left:150px;
+}
     </style>
 
 </head>
@@ -36,11 +40,35 @@
 <?php include 'nav.php'; ?>
 
 <div class="par">
-<h1> <!-- Header goes here --> </h1>
+<h1> History </h1>
 
 <pre class="content">
 
-<!-- Text goes here -->
+
+
+The Coders club was born under the co-founders: Katherine Moloney and Thomas “Chuck” Griffin. Their 
+idea was to create an organization at the City College of San Francisco where students can share advice 
+about programming, be social and get together to hack.
+
+The first meeting held on August 27th, 2012. Online presence was established early on, 
+ensuring ongoing contact between club members and alumni.
+
+Spring 2013 was first semester for the Coders club to be officially recognized by CCSF . Meetings
+were often held alongside the Linux Users Group (LUG), a club that Coders will always have close 
+ties with. Katherine Moloney was the first president of the Coders club, followed by Thomas “Chuck” 
+Griffin as the second president in Fall 2013.
+
+Even though both co-founders are both employed full-time in the tech industry, they continually give 
+back to Coders, providing insight and mentoring new leaders for programming careers in the San Francisco Bay Area.
+
+Furthermore, Katherine established the Women-Who-Code club at CCSF, modeled after the 
+Women-Who-Code-SF in order to support women interested in computer science at our community college.
+
+Lastly, the Coders club will always strive to motivate students to embrace the bustling tech community 
+we live in, giving back to it whenever possible.
+
+Coding is fun but coding together is awesome!
+
 
 </pre>
 
