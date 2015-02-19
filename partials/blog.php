7a23aa5c9de6dbaf647954e480d56dd11ca6db70
Tyler Brothers <tbro@pl.local>
2015-02-18 16:07:22 -0800

    Removed heroku files. Fixed spelling and nav bar naming.

--- partials/blog.php
+++ partials/blog.php
@@ -4,11 +4,11 @@
 
     <p class="body">
       So we have a new website! Rebuilding it was a learning experience for me and hopefully improved 
-      the look and feel of the website. With this first blog post I’d like to list what I 
+      the look and feel of the website. With this first blog post I'd like to list what I 
       hoped to accomplish. Of course the website should be a landing page for anyone who is looking up 
       coders club to attend. I think it would be nice if it had some content that people (member and non 
       members) could come back to enjoy. I would like it to express who we are more than with a 
-      statement but with style, as well serve as a playground for club members. I’d like to see changes 
+      statement but with style, as well serve as a playground for club members. I'd like to see changes 
       made weekly contributed by different members.
     </p>
   </div>
@@ -18,7 +18,7 @@
     <p class="body">
       The blog is self explanatory. Basically I think every member should. Be able to create blog posts, 
       the content of which can be anything they want and occasionally outlining what were doing as a organisation, 
-      for example I’m sure there would be a post about open hatch after it happens. 
+      for example I'm sure there would be a post about open hatch after it happens. 
 
       How it will work technically has yet to be determined. Kevin and I plan to build a web based blog 
       application, where you can post in a form like you would post to Tumblr, or Wordpress. And I don't particularly 
@@ -31,7 +31,7 @@
   <div class="post">
     <h2>The Toy Box</h2>
     <p class="body">
-      The Toy Box is my favorite idea I’ve had for the website. Basically the point is to provide a place 
+      The Toy Box is my favorite idea I've had for the website. Basically the point is to provide a place 
       for Club members to showcase the projects they have worked on. What differentiates it from the blog 
       is that, it isn't just going to talk about the things we have made, its going have them 
       accessible for you to play with. 
--- partials/nav.php
+++ partials/nav.php
@@ -4,9 +4,9 @@
 
     <li onclick="link('index.php', false)">Home</li>
     <li onclick="link('index.php#events', false)">Event</li>
-    <li onclick="link('index.php#contact', false)">Event</li>
+    <li onclick="link('index.php#contact', false)">Contact</li>
     <li onclick="link('blog.php', false)">Blog</li>
-    <li onclick="link('https://www.nixcode.us/irc', true)">Blog</li>
+    <li onclick="link('https://www.nixcode.us/irc', true)">IRC</li>
 
   </ul>
 </nav>
