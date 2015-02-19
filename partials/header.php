440bfcfaf21c108531264dc063cb1df1fea1d50d
Tyler Brothers <tylerbrothers1@gmail.com>
2015-02-18 16:17:11 -0800

    Added the 's' to 'https' to off-site scripts and styles.

--- partials/header.php
+++ partials/header.php
@@ -5,13 +5,13 @@
   <title>CCSF Coders Club</title>
 
   <!-- Styles begin-->
-  <link href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' rel='stylesheet'>
-  <link href='http://fonts.googleapis.com/css?family=Share+Tech+Mono:400' rel='stylesheet' type='text/css'>
+  <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family=Share+Tech+Mono:400' rel='stylesheet' type='text/css'>
   <link href='../styles/style.css' rel='stylesheet' type='text/css'>
   <!-- Styles end -->
 
   <!-- Scripts begin-->
-  <script src='http://code.jquery.com/jquery-2.1.3.min.js'></script>
+  <script src='https://code.jquery.com/jquery-2.1.3.min.js'></script>
   <script src='../scripts/link.js' type="text/javascript"></script>
   <script src='../scripts/quick.js' type="text/javascript"></script>
   <!-- Scripts end -->
