e4bdb4a1ff82e4a81382a830d9ff9c76d1646bd6
Tyler Brothers <tylerbrothers1@gmail.com>
2015-03-25 19:44:21 -0700

    basictests.py: updated tests to output more meaningful error messages

--- tests/basictests.py
+++ tests/basictests.py
@@ -4,56 +4,63 @@ from selenium import webdriver
 class TestCodersMainPage(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
-        self.driver.get('localhost:80')
+        self.driver.get('localhost:6969')
 
     def test_SocialMediaLinksPresent(self):
-        linkToGitHub = self.driver.find_elements_by_id('social-github')
-        self.assertTrue(linkToGitHub[0])
+        linkToGitHub = self.driver.find_element_by_id('social-github')
+        self.assertTrue('social-github' in linkToGitHub.get_attribute('id'))
 
-        linkToFaceBook = self.driver.find_elements_by_id('social-facebook')
-        self.assertTrue(linkToFaceBook[0])
+        linkToFaceBook = self.driver.find_element_by_id('social-facebook')
+        self.assertTrue('social-facebook' in linkToFaceBook.get_attribute('id'))
 
-        linkToYouTube = self.driver.find_elements_by_id('social-youtube')
-        self.assertTrue(linkToYouTube[0])
+        linkToYouTube = self.driver.find_element_by_id('social-youtube')
+        self.assertTrue('social-youtube' in linkToYouTube.get_attribute('id'))
 
-        linkToGoogleGroup = self.driver.find_elements_by_id('social-googlegroup')
-        self.assertTrue(linkToGoogleGroup[0])
+        linkToGoogleGroup = self.driver.find_element_by_id('social-googlegroup')
+        self.assertTrue('social-googlegroup' in linkToGoogleGroup.get_attribute('id'))
 
-        linkToTwitter = self.driver.find_elements_by_id('social-twitter')
-        self.assertTrue(linkToTwitter[0])
+        linkToTwitter = self.driver.find_element_by_id('social-twitter')
+        self.assertTrue('social-twitter' in linkToTwitter.get_attribute('id'))
 
     def test_MeetingInfoIsPresent(self):
-        meetingTimes = self.driver.find_elements_by_id('meetingtimes')
-        self.assertTrue(meetingTimes[0])
+        meetingTimes = self.driver.find_element_by_id('meetingtimes')
+        self.assertTrue('meetingtimes' in meetingTimes.get_attribute('id'))
 
-        meetingRooms = self.driver.find_elements_by_id('meetingrooms')
-        self.assertTrue(meetingRooms[0])
+        meetingRooms = self.driver.find_element_by_id('meetingrooms')
+        self.assertTrue('meetingrooms' in meetingRooms.get_attribute('id'))
 
     def test_navBarLinksArePresent(self):
-        homeLink = self.driver.find_elements_by_id('nav-home')
-        self.assertTrue(homeLink[0])
+        homeLink = self.driver.find_element_by_id('nav-home')
+        self.assertTrue('nav-home' in homeLink.get_attribute('id'))
 
-        eventsLink = self.driver.find_elements_by_id('nav-events')
-        self.assertTrue(eventsLink[0])
+        eventsLink = self.driver.find_element_by_id('nav-events')
+        self.assertTrue('nav-events' in eventsLink.get_attribute('id'))
 
-        contactLink = self.driver.find_elements_by_id('nav-contact')
-        self.assertTrue(contactLink[0])
+        contactLink = self.driver.find_element_by_id('nav-contact')
+        self.assertTrue('nav-contact' in contactLink.get_attribute('id'))
         
-        blogLink = self.driver.find_elements_by_id('nav-blog')
-        self.assertTrue(blogLink[0])
+        blogLink = self.driver.find_element_by_id('nav-blog')
+        self.assertTrue('nav-blog' in blogLink.get_attribute('id'))
 
-        ircLink = self.driver.find_elements_by_id('nav-irc')
-        self.assertTrue(ircLink[0])
+        ircLink = self.driver.find_element_by_id('nav-irc')
+        self.assertTrue('nav-irc' in ircLink.get_attribute('id'))
 
     def test_calendarIsPresent(self):
-        eventsCalendar = self.driver.find_elements_by_id('events-calendar')
-        self.assertTrue(eventsCalendar[0])
+        eventsCalendar = self.driver.find_element_by_id('events-calendar')
+        self.assertTrue('events-calendar' in eventsCalendar.get_attribute('id'))
 
     def test_contactsArePresent(self):
-        presidentContact = self.driver.find_elements_by_id('contacts-president')
-        advisorContact = self.driver.find_elements_by_id('contacts-advisor')
-        treasurerContact = self.driver.find_elements_by_id('contacts-treasurer')
-        codersClubEmail = self.driver.find_elements_by_id('contacts-codersemail')
+        presidentContact = self.driver.find_element_by_id('contacts-president')
+        self.assertTrue('contacts-president' in presidentContact.get_attribute('id'))
+
+        advisorContact = self.driver.find_element_by_id('contacts-advisor')
+        self.assertTrue('contacts-advisor' in advisorContact.get_attribute('id'))
+
+        treasurerContact = self.driver.find_element_by_id('contacts-treasurer')
+        self.assertTrue('contacts-treasurer' in treasurerContact.get_attribute('id'))
+
+        codersClubEmail = self.driver.find_element_by_id('contacts-codersemail')
+        self.assertTrue('contacts-codersemail' in codersClubEmail.get_attribute('id'))
 
     def tearDown(self):
         self.driver.close()
