256bb8fc21da782a4f007c1929fd21c25a6070d7
Tyler Brothers <tylerbrothers1@gmail.com>
2015-06-15 13:32:14 -0700

    coding: first draft of challenge listing. First challenge added

--- partials/codingchallengescontent.php
+++ partials/codingchallengescontent.php
@@ -1,2 +1,57 @@
 <!-- Begin codingchallengescontent.php -->
-<!-- End codingchallengescontent.php -->
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="../styles/codingchallenges.css">
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12 text-center" style="margin-top: 60px;">
+      <h2>Coding Challenges</h2>
+      <p>
+        Written by club members and implemented in any language they prefer, coding challenges provide a
+        creative outlet for many of our members to invent solutions to atypical problems. The challenges
+        are presented once a week, usually at the end of a club meeting. The challenges can be proposed by anyone
+        but usually a club officer will prepare a challenge in advance. Occasionally, "nightmare modes" will be
+        available for certain challenges, offering a higher level of difficulty due to some type of restriction to
+        the type of answers that "count." 
+      </p>
+      <p>
+        Anyone can participate in challenges, including non-club members! As Drew Carey says "Everything is made up,
+        and the points don't matter!"
+      </p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-12 text-center" style="margin-top: 60px;">
+      <h2>Challenge Listing</h2>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-4 card-container">
+      <div style="background-color:#00FF00;" class="text-center">
+        <h2 style="color:black;">2+2=5</h2>
+      </div>
+      <div style="background-color:#00FF00;padding:20px;" class="text-center">
+        <p style="color:black;">
+          Write code in any language you like that makes it look like you are adding 2+2 but the evaluation 
+          of your code outputs 5. The goal is to make the code hide the fact that its manipulating the answer 
+          to be 5. The trickier and more subtle your code is, the better.
+        </p>
+        <div class="row">
+          <div class="col-lg-6">
+            <button type="button" class="btn btn-default btn-lg" style="margin-top:20px;">
+              <a href="https://groups.google.com/forum/#!searchin/ccsfcoders/coding$20challenge/ccsfcoders/f-s5k5BvNh4/BUJSZRkegAkJ">Discussion</a>
+            </button>
+          </div>
+          <div class="col-lg-6">
+            <button type="button" class="btn btn-default btn-lg" style="margin-top:20px;">
+              <a href="http://code.runnable.com/u/codersclub">Submissions</a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- End codingchallengescontent.php
