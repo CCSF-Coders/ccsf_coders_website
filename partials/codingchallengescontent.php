893ead0b3e8b34d707722713ed48007b3e0b2966
Tyler Brothers <tylerbrothers1@gmail.com>
2015-06-15 15:10:10 -0700

    coding: challenges added. All submissions are on the website Runnable and linked in the page. All discussions are linked.

--- partials/codingchallengescontent.php
+++ partials/codingchallengescontent.php
@@ -26,7 +26,40 @@
       <h2>Challenge Listing</h2>
     </div>
   </div>
+  <!-- 3 coding challenges per row. -->
   <div class="row">
+    <!-- coding challenge card starts here -->
+    <div class="col-lg-4 card-container">
+      <div class="card-title text-center">
+        <h2>Hello World</h2>
+      </div>
+      <div class="card-content text-center">
+        <p>
+          In any language you prefer, write a script that produces the text "Hello World" on to the screen in 
+          some fashion. Lowest character count is the goal!
+        </p>
+        <p>
+          Rules: The character count will begin with the characters in the executed script file. 
+          Only 1 script file (e.g. "helloworld.java") and 1 execution script (e.g. "python helloworld.py") will be accepted.
+          Characters include, but are not limited to, spaces, apostrophes, etc. Trigraphs and digraphs will only count as 
+          the amount of characters that are physically present. "HW" programming language banned.
+        </p>
+        <div class="row">
+          <div class="col-lg-6">
+            <button type="button" class="btn btn-default btn-lg">
+              <a href="https://groups.google.com/forum/#!msg/ccsfcoders/_sPeFUj6tso/cXW7SKO_7ZcJ">Discussion</a>
+            </button>
+          </div>
+          <div class="col-lg-6">
+            <button type="button" class="btn btn-default btn-lg">
+              <a href="http://code.runnable.com/u/codersclub">Submissions</a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- coding challenge card ends here -->
+    <!-- coding challenge card starts here -->
     <div class="col-lg-4 card-container">
       <div class="card-title text-center">
         <h2>2+2=5</h2>
@@ -51,6 +84,76 @@
         </div>
       </div>
     </div>
+    <!-- coding challenge card ends here -->
+    <!-- coding challenge card starts here -->
+    <div class="col-lg-4 card-container">
+      <div class="card-title text-center">
+        <h2>FizzBuzz</h2>
+      </div>
+      <div class="card-content text-center">
+        <p>
+          Write FizzBuzz for the numbers 0 - 99. A working problem will<br>
+          1) display 'Fizz' if the current number is divisible by three,<br>
+          2) display 'Buzz' if the current number is divisible by five,<br> 
+          3) display 'FizzBuzz' if the number is divisible by both three and five, otherwise<br>
+          4) display the number.
+        </p>
+        <p>
+          The only metric is whether your code works or not. Bonus challenge: shortest character count 
+        </p>
+        <div class="row">
+          <div class="col-lg-6">
+            <button type="button" class="btn btn-default btn-lg">
+              <a href="https://groups.google.com/forum/#!searchin/ccsfcoders/coding$20challenge/ccsfcoders/SO3be-1s98c/DDTwkdHDeWQJ">Discussion</a>
+            </button>
+          </div>
+          <div class="col-lg-6">
+            <button type="button" class="btn btn-default btn-lg">
+              <a href="http://code.runnable.com/u/codersclub">Submissions</a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- coding challenge card ends here -->
+  </div>
+  <!-- 3 coding challenges per row. -->
+  <div class="row">
+    <!-- coding challenge card starts here -->
+    <div class="col-lg-4 card-container">
+      <div class="card-title text-center">
+        <h2>Collatz Conjecture</h2>
+      </div>
+      <div class="card-content text-center">
+        <p> 
+          The Collatz Conjecture states that<br><br>
+
+          Start with an integer n > 1.<br>
+          Repeat the following steps:<br>
+          If n is even, divide it by 2.<br>
+          If n is odd, multiply it by 3<br> and add 1.<br><br>
+
+          It is proven that for all positive integers up to 5 * 260, or about 5764000000000000000, n will eventually become 1.
+        </p>
+        <p>
+          Relevant <a href="http://codegolf.stackexchange.com/questions/12177/collatz-conjecture">code golf challenge.</a><br>
+          Relevant <a href="http://xkcd.com/710/">xkcd</a>
+        </p>
+        <div class="row">
+          <div class="col-lg-6">
+            <button type="button" class="btn btn-default btn-lg">
+              <a href="https://groups.google.com/forum/#!searchin/ccsfcoders/collatz$20conjecture/ccsfcoders/rOKpYGRiypQ/AB6rk-eGcWgJ">Discussion</a>
+            </button>
+          </div>
+          <div class="col-lg-6">
+            <button type="button" class="btn btn-default btn-lg">
+              <a href="http://code.runnable.com/u/codersclub">Submissions</a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- coding challenge card ends here -->
   </div>
 </div>
 
