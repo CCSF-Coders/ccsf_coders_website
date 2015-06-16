75307b16c0959dde708ebe0addde190b7e889822
Tyler Brothers <tylerbrothers1@gmail.com>
2015-06-16 09:46:19 -0700

    updated nav to work a bit better, the button method did not allow someone to click the button and follow the link

--- partials/nav.php
+++ partials/nav.php
@@ -3,27 +3,13 @@
   <div class="col-lg-12">
     <nav class="text-center">
 
-        <button type="button" class="btn btn-lg">
           <a href="index.php" id='nav-home'>Home</a>
-        </button>
-        <button type="button" class="btn btn-lg">
           <a href="index.php#events" id='nav-events'>Events</a>
-        </button>
-        <button type="button" class="btn btn-lg">
           <a href="index.php#contact" id='nav-contact'>Contact</a>
-        </button>
-        <button type="button" class="btn btn-lg">
           <a href="history.php" id='nav-history'>History</a>
-        </button>
-        <button type="button" class="btn btn-lg">
           <a href="codingchallenges.php" id='nav-codingchallenges'>Coding Challenges</a>
-        </button>
-        <button type="button" class="btn btn-lg">
           <a href="blog.php" id='nav-blog'>Blog</a>
-        </button>
-        <button type="button" class="btn btn-lg">
           <a href="https://www.nixcode.us/irc" id='nav-irc' target="_blank">IRC</a>
-        </button>
 
     </nav>
   </div>
