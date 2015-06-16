c085c0d8b1c5b3a084d97fc01044b04bf486b600
Tyler Brothers <tylerbrothers1@gmail.com>
2015-06-16 09:51:36 -0700

    bootstrapified events

--- partials/events.php
+++ partials/events.php
@@ -1,15 +1,17 @@
 <!-- Begin events.php -->
 <!-- Event calendar and description section -->
-<div class="section" id="events">
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12 text-center">
 
-	<h1>Upcoming Events</h1>
-	<p>
-	Stop in on thursday for our weekly hang out at Batmale 453.
-	</p>
+      <h1>Upcoming Events</h1>
+      <p>
+      Check back here when the semester starts for our calendar.
+      </p>
 
-	<iframe id='events-calendar' src="https://www.google.com/calendar/embed?src=coders%40mail.ccsf.edu&ctz=America/Los_Angeles" style="border: 0" width="640" height="480" frameborder="0" scrolling="no"></iframe>
+      <iframe id='events-calendar' src="https://www.google.com/calendar/embed?src=coders%40mail.ccsf.edu&ctz=America/Los_Angeles" style="border: 0" width="640" height="480" frameborder="0" scrolling="no"></iframe>
 
-	<?php require_once('upsydownsy.php'); ?>
-
+    </div>
+  </div>
 </div>
 <!-- End events.php -->
