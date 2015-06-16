b4c55231b127eafc3455de4a05e1476b01b82663
Tyler Brothers <tylerbrothers1@gmail.com>
2015-06-16 11:21:02 -0700

    nav and contact section are now mobile-ok

--- partials/index-content.php
+++ partials/index-content.php
@@ -42,45 +42,49 @@
 
 <link rel="stylesheet" type="text/css" href="../styles/index-style.css">
 
-<div class="container" id="contact">
+<div class="container">
   <!-- Individuals contact -->
   <div class="row">
-    <div class="col-lg-12 text-center">
+    <div class="col-xs-12 text-center" id="contact">
 
       <h1>Get In Touch</h1>
       <!-- Contact headings -->
       <div class="row titles">
-        <div class="col-lg-2 col-lg-offset-3"><p>Name</p></div>
-        <div class="col-lg-2"><p>Title</p></div>
-        <div class="col-lg-2"><p>Link</p></div>
+        <div class="col-xs-2 col-xs-offset-3"><p>Name</p></div>
+        <div class="col-xs-2"><p>Title</p></div>
+        <div class="col-xs-2"><p>Link</p></div>
       </div>
       <!-- Contact 1 -->
       <div class="row">
-        <div class="col-lg-2 col-lg-offset-3"><p>TBD</p></div>
-        <div class="col-lg-2"><p>President</p></div>
-        <div class="col-lg-2"><a href="mailto:coders%40mail.ccsf.edu">coders@mail.ccsf.edu</a></div>
+        <div class="col-xs-2 col-xs-offset-3"><p>TBD</p></div>
+        <div class="col-xs-2"><p>President</p></div>
+        <div class="col-xs-2"><a href="mailto:coders%40mail.ccsf.edu">coders@mail.ccsf.edu</a></div>
       </div>
       <!-- Contact 2 -->
       <div class="row">
-        <div class="col-lg-2 col-lg-offset-3"><p>TBD</p></div>
-        <div class="col-lg-2"><p>Advisor</p></div>
-        <div class="col-lg-2"><p>TBD</p></div>
+        <div class="col-xs-2 col-xs-offset-3"><p>TBD</p></div>
+        <div class="col-xs-2"><p>Advisor</p></div>
+        <div class="col-xs-2"><p>TBD</p></div>
       </div>
       <!-- Contact 3 -->
       <div class="row">
-        <div class="col-lg-2 col-lg-offset-3"><p>TBD</p></div>
-        <div class="col-lg-2"><p>Treasurer</p></div>
-        <div class="col-lg-2"><p>TBD</p></div>
+        <div class="col-xs-2 col-xs-offset-3"><p>TBD</p></div>
+        <div class="col-xs-2"><p>Treasurer</p></div>
+        <div class="col-xs-2"><p>TBD</p></div>
       </div>
       <!-- Contact 4 -->
       <div class="row">
-        <div class="col-lg-2 col-lg-offset-3"><p>TBD</p></div>
-        <div class="col-lg-2"><p>Officer</p></div>
-        <div class="col-lg-2"><p>TBD</p></div>
+        <div class="col-xs-2 col-xs-offset-3"><p>TBD</p></div>
+        <div class="col-xs-2"><p>Officer</p></div>
+        <div class="col-xs-2"><p>TBD</p></div>
       </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-12 text-center">
       <!-- Further contact -->
       <div class="row" style="margin-top:60px;">
-        <div class="col-lg-12">
+        <div class="col-xs-12">
           <p>
             The best way to get in contact with the group is to join our 
             <a href="https://groups.google.com/forum/#!forum/ccsfcoders" target="_blank">Google Group!</a>
--- partials/nav.php
+++ partials/nav.php
@@ -1,17 +1,17 @@
 <!-- Begin nav.php -->
-<div class="row">
-  <div class="col-lg-12">
-    <nav class="text-center">
-
-          <a href="index.php" id='nav-home'>Home</a>
-          <a href="index.php#events" id='nav-events'>Events</a>
-          <a href="index.php#contact" id='nav-contact'>Contact</a>
-          <a href="history.php" id='nav-history'>History</a>
-          <a href="codingchallenges.php" id='nav-codingchallenges'>Coding Challenges</a>
-          <a href="blog.php" id='nav-blog'>Blog</a>
-          <a href="https://www.nixcode.us/irc" id='nav-irc' target="_blank">IRC</a>
-
-    </nav>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12 text-center">
+      <nav>
+        <a href="index.php" id='nav-home'>Home</a>
+        <a href="index.php#events" id='nav-events'>Events</a>
+        <a href="index.php#contact" id='nav-contact'>Contact</a>
+        <a href="history.php" id='nav-history'>History</a>
+        <a href="codingchallenges.php" id='nav-codingchallenges'>Coding Challenges</a>
+        <a href="blog.php" id='nav-blog'>Blog</a>
+        <a href="https://www.nixcode.us/irc" id='nav-irc' target="_blank">IRC</a>
+      </nav>
+    </div>
   </div>
 </div>
 <!-- End nav.php -->
