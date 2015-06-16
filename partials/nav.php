4686c2fcd88f39780f5e80ab8eac18a7aabae0a0
Tyler Brothers <tylerbrothers1@gmail.com>
2015-06-16 11:32:18 -0700

    removed blog.php from nav bar until William or Kevin finishes it

--- partials/nav.php
+++ partials/nav.php
@@ -8,7 +8,8 @@
         <a href="index.php#contact" id='nav-contact'>Contact</a>
         <a href="history.php" id='nav-history'>History</a>
         <a href="codingchallenges.php" id='nav-codingchallenges'>Coding Challenges</a>
-        <a href="blog.php" id='nav-blog'>Blog</a>
+        <!-- Blog was never finished by William, so Tyler took it off the navigation bar. -->
+        <!-- <a href="blog.php" id='nav-blog'>Blog</a> -->
         <a href="https://www.nixcode.us/irc" id='nav-irc' target="_blank">IRC</a>
       </nav>
     </div>
