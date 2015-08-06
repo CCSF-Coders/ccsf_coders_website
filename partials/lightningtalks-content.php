d8ee96131d7d2e7bc73bf217491837e863541bea
Tyler Brothers <tylerbrothers1@gmail.com>
2015-08-06 10:53:07 -0700

    Added the lightning talk page and nav bar button
    * page that describes what a lightning talk is and how to get involved.
    * responsive play list video from our youtube channel. Always up to date!

---
+++ partials/lightningtalks-content.php
@@ -0,0 +1,43 @@
+<!-- Begin lightningtalks-content.php -->
+<link rel="stylesheet" type="text/css" href="../styles/codingchallenges-style.css">
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12 text-center">
+      <h1>Lightning Talk?</h1>
+      <p>
+        Lightning Talks are student delivered speeches that can be about anything that gets the speaker excited.
+        The catch: The speech can <i>only</i> be 10 minutes long. You will be surprised at how quickly 10 minutes disappears when you
+        are in front of a group of people, so we ask that speakers come prepared with some type of PowerPoint-like
+        slide show.
+      </p>
+      <p>
+        Anyone can participate in challenges, including non-club members! As Drew Carey says "Everything is made up,
+        and the points don't matter!" To participate, feel free to join us on the Google Group and make a new thread
+        with your idea for a speech.
+      </p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-12 text-center">
+      <h1>View them on our YouTube channel</h1>
+    </div>
+
+    <div class="row">
+      <!-- lightning talk card starts here -->
+      <div class="col-lg-8 col-lg-offset-2 hidden-xs card-container">
+        <div class="card-content text-center">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLyGLemjnH3ukTGW8TYCzdu7jQe0U-wEKi" frameborder="0" allowfullscreen></iframe>
+        </div>
+      </div>
+      <div class="col-xs-8 col-xs-offset-2 visible-xs card-container">
+        <div class="card-content text-center">
+        <a href="https://www.youtube.com/playlist?list=PLyGLemjnH3ukTGW8TYCzdu7jQe0U-wEKi">Link to our YouTube Lightning Talk play list</a>
+        </div>
+      </div>
+      <!-- lightning talk card ends here -->
+    </div>
+  </div>
+</div>
+
+<!-- End lightningtalks-content.php
---
+++ templates/lightningtalks.php
@@ -0,0 +1,13 @@
+<!-- Begin lightningtalks.php -->
+<?php 
+
+$templateDirectory = '../partials/';
+
+require_once($templateDirectory.'header.php');
+
+require_once($templateDirectory.'lightningtalks-content.php');
+
+require_once($templateDirectory.'footer.php'); 
+
+?>
+<!-- End lightningtalks.php -->
