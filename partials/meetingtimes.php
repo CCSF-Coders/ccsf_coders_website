4cb55cda635fcb90fdd6af137577dad7afc555c1
kmolo <kmolo@users.noreply.github.com>
2015-09-04 17:27:39 -0700

    update Meeting Times for Fall 2015

--- partials/meetingtimes.php
+++ partials/meetingtimes.php
@@ -2,8 +2,8 @@
 <div class="info-wrapper" id="meetingtimes">
   <h1>Meeting Times</h1>
   <div class="info" id="meetingrooms">
-    Time: TBD<br>
-    Room: TBD
+    Thursdays, 5PM - 7PM<br>
+    Batmale Hall, Rm 453
   </div>
 </div>
 <!-- End meetingtimes.php -->
