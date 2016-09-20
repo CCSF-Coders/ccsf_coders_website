4323cf33f9831437ed7c98942d902142853bf932
Tyler Brothers <tylerbrothers1@gmail.com>
2016-09-19 18:24:54 -0700

    Added the slack channel to the social links.

--- partials/quicklinks.php
+++ partials/quicklinks.php
@@ -7,5 +7,6 @@
   <a href="https://www.youtube.com/user/CCSFCoders" target="_blank" id="social-youtube"><i class="fa fa-youtube-square"></i></a>
   <a href="https://groups.google.com/forum/#!forum/ccsfcoders" target="_blank" id="social-googlegroup"><i class="fa fa-google"></i></a>
   <a href="https://twitter.com/ccsfcoders" target="_blank" id="social-twitter"><i class="fa fa-twitter-square"></i></a>
+  <a href="https://ccsfcodersclub.slack.com" target="_blank" id="social-slack"><i class="fa fa-slack" aria-hidden="true"></i></a>
 </div>
 <!-- End quicklinks.php -->
