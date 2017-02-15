f019894e316de37e5e59cdb91dd70a7dd007d326
Jack Cole <jack-cole@users.noreply.github.com>
2017-02-15 13:27:54 -0800

    Update spring 17 (#36)
    * Updated for Spring 2017 Semester
    
    * more updates

--- partials/meetingtimes.php
+++ partials/meetingtimes.php
@@ -1,9 +1,9 @@
 <!-- Begin meetingtimes.php -->
 <div class="info-wrapper" id="meetingtimes">
-  <h1>Meeting Times<br/>(bi-weekly)</h1>
+  <h1>Spring 17 Meeting Times<br/>(weekly)</h1>
   <div class="info" id="meetingrooms">
-    Fridays, 3-5 PM<br>
-    Batmale Hall, Rm 453
+    Wednesdays, 4-6 PM<br>
+    Batmale Hall, Rm 411
   </div>
 </div>
 <!-- End meetingtimes.php -->
