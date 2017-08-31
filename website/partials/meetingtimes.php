2b537772dd92358f9d1bb2945c51438f9b9abdf6
Elias Lawson-Fox <eliaslfox@gmail.com>
2017-08-30 23:38:33 -0700

    removed bugs

--- website/partials/meetingtimes.php
+++
@@ -1,9 +0,0 @@
-<!-- Begin meetingtimes.php -->
-<div class="info-wrapper" id="meetingtimes">
-  <h1>Spring 17 Meeting Times<br/>(weekly)</h1>
-  <div class="info" id="meetingrooms">
-    Wednesdays, 4-6 PM<br>
-    Batmale Hall, Rm 411
-  </div>
-</div>
-<!-- End meetingtimes.php -->
